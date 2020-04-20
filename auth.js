911618d78248ea2c4157153be1d9fcaaaafbe1a6
laurion <klauriondareich@gmail.com>
2020-04-20 02:47:53 +0100

    signin and register implemented

--- auth.js
+++ auth.js
@@ -1,12 +1,26 @@
 // Firabase Authentication implementation
 const Auth = firebase.auth();
 
-function signIn(){
+// Allows to sign up the user
+function register(){
  let email = document.getElementById("email").value;
  let password = document.getElementById("password").value;
+ console.log(`${email} and ${password}`);
  Auth.createUserWithEmailAndPassword(email, password).then(() =>{
-   console.log("succes ! user added")
- }).catch(() =>{
-   console.error("error ! user has not been added")
+   alert("User added")
+ }).catch((error) =>{
+   console.error(error.message)
  })
 };
+
+// Allows to sign in the user
+function signIn(){
+  let email = document.getElementById("signinEmail").value;
+  let password = document.getElementById("signinPassword").value;
+  console.log(`${email} and ${password}`);
+  Auth.signInWithEmailAndPassword(email, password).then(() =>{
+    alert("signin with success")
+  }).catch((error) =>{
+    console.error(error.message)
+  })
+ };
