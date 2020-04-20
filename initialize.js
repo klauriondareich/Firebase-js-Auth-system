80773ecd6990fbdefdf7b64d6f2bfedce8dc2e16
laurion <klauriondareich@gmail.com>
2020-04-20 01:23:52 +0100

    init

---
+++ initialize.js
@@ -0,0 +1,13 @@
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+  apiKey: "AIzaSyCSfNbNYyc9uXQcqUAuTuaZUzPRx_qycDM",
+  authDomain: "firebasqueries.firebaseapp.com",
+  databaseURL: "https://firebasqueries.firebaseio.com",
+  projectId: "firebasqueries",
+  storageBucket: "firebasqueries.appspot.com",
+  messagingSenderId: "686095469091",
+  appId: "1:686095469091:web:901c75b38e878047b33f96"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
