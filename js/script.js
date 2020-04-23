96a416bed046fca89897aeac6e91a5d6d3b9f6cb
laurion <klauriondareich@gmail.com>
2020-04-23 21:59:48 +0100

    upload and download data form cloud storagedone

--- js/script.js
+++ js/script.js
@@ -19,3 +19,30 @@ modelRef.doc("set3").set({
   configType: "Large",
   state: true,
 });
+
+// Cloud Storage Initialization
+
+let Storage = firebase.storage().ref();
+let metadata = {
+  ContentLanguage : "fr",
+  contentType: "image/jpeg"
+}
+
+// Allows to store images in the firebase storage
+function storeImage(){
+  let file = document.getElementById("upload-avatar").files[0];
+  Storage.child("avatar/" + file.name).put(file, metadata)
+  .then(() =>{
+    alert("file uploaded");
+    getStoredImage(file.name)
+  }).catch((error) =>{
+    alert(error.message)
+  })
+}
+
+// Allows getting images from firebase storage
+function getStoredImage(url){
+  Storage.child("avatar/" + url).getDownloadURL().then((url) =>{
+    document.getElementById("avatar").src = url
+  })
+}
